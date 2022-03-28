a1ecb8a77dc8a70cec3d214c08524c095d0bd2d6
programer10 <programer10@ucione.local>
2022-03-28 20:24:12 +0200

    first commit

---
+++ src/Components/Message.js
@@ -0,0 +1,28 @@
+    const Message = ({ messages, users }) => {
+    console.log(messages)
+
+  return (
+    <div>
+      <ul className="Messages-list">
+        {messages.map((message) => (
+          <div key={messages.indexOf(message)} 
+          className={(message.chatUserID === users)
+            ? "Messages-message"
+            : "Messages-message currentMember"}>
+            <span
+              className="avatar"
+              style={{ backgroundColor: `${message.userColor}` }}
+            />
+            <div className="Message-content">
+              <div className="username">{message.username}</div>
+              <div className="text">{message.text}</div>
+            </div>
+            
+          </div>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Message;
